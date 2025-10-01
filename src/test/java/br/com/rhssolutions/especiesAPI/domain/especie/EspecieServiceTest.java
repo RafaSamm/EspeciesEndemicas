18333590449b041a66a585bd022db2bbe8ac3636
Rafael Sammarco <126182047+RafaSamm@users.noreply.github.com>
2025-10-01 10:59:19 -0300

    test: mais funcionalidades do EspecieService

--- src/test/java/br/com/rhssolutions/especiesAPI/domain/especie/EspecieServiceTest.java
+++ src/test/java/br/com/rhssolutions/especiesAPI/domain/especie/EspecieServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static br.com.rhssolutions.especiesAPI.common.EspecieFactory.sincronizarEspecieValidaDTO;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -60,6 +61,87 @@ public class EspecieServiceTest {
         //Verifica se o repository nunca foi chamado
         verify(especieRepository, never()).saveAll(anyList());
 
+    }
+
+    @Test
+    public void buscarApiEspecieAleatoria() {
+        EspecieDTO especieDTO = sincronizarEspecieValidaDTO();
+        var especieConvertida = EspecieMapper.toDomain(especieDTO);
+
+        when(aesClient.getRandomEspecie()).thenReturn(especieDTO);
+        when(especieRepository.save(any())).thenReturn(especieConvertida);
+
+        var sut = especieService.buscarEspecieAleatoria();
+
+        assertThat(sut).isNotNull();
+        assertThat(sut).isEqualTo(especieConvertida);
+    }
+
+    @Test
+    public void buscarApiEspecieAleatoriaInvalida() {
+        when(aesClient.getRandomEspecie()).thenThrow(new EspecieNotFoundException("API indisponível"));
+
+        // Testando o service se está lançando a exceção
+        assertThatThrownBy(() -> especieService.buscarEspecieAleatoria())
+                .isInstanceOf(EspecieNotFoundException.class)
+                .hasMessageContaining("Erro ao buscar especie aleatoria");
+
+        //Verifica se o repository nunca foi chamado
+        verify(especieRepository, never()).save(any());
+    }
+
+    @Test
+    public void listarTodasAsEspecies() {
+        EspecieDTO especieDTO = sincronizarEspecieValidaDTO();
+        var especieConvertida = EspecieMapper.toDomain(especieDTO);
+
+        when(especieRepository.findAll()).thenReturn(List.of(especieConvertida));
+
+        var sut = especieService.listaTodasAsEspecies();
+
+        assertThat(sut).isNotNull();
+        assertThat(sut).isNotEmpty();
+        assertThat(sut).hasSize(1);
+        assertThat(sut).containsExactly(especieConvertida);
+        assertThat(sut).isEqualTo(List.of(especieConvertida));
+    }
+
+    @Test
+    public void listarTodasAsEspeciesVazia() {
+        when(especieRepository.findAll())
+                .thenThrow(new EspecieNotFoundException("Nao ha especies Cadastradas"));
+
+        assertThatThrownBy(() -> especieService.listaTodasAsEspecies())
+                .isInstanceOf(EspecieNotFoundException.class)
+                .hasMessageContaining("Nao ha especies Cadastradas");
+
+        verify(especieRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void buscarEspeciePorId() {
+        EspecieDTO especieDTO = sincronizarEspecieValidaDTO();
+        var especieConvertida = EspecieMapper.toDomain(especieDTO);
+
+        when(especieRepository.findById(anyLong())).thenReturn(Optional.of(especieConvertida));
+
+        var sut = especieService.buscarEspeciePorId(1L);
+
+        assertThat(sut).isNotNull();
+        assertThat(sut).isPresent();
+        assertThat(sut).isEqualTo(Optional.of(especieConvertida));
+    }
+
+    @Test
+    public void buscarEspeciePorIdInvalido() {
+        when(especieRepository.findById(anyLong()))
+                .thenThrow(new EspecieNotFoundException("Especie nao encontrada"));
+
+        assertThatThrownBy(() -> especieService.buscarEspeciePorId(1L))
+                .isInstanceOf(EspecieNotFoundException.class)
+                .hasMessageContaining("Especie nao encontrada");
+
+        verify(especieRepository, times(1)).findById(anyLong());
     }
 
 
